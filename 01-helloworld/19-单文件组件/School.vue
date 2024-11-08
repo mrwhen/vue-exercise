4cc62cb1f5776aeaf2b9d5746dae0cb69f28537c
sean <ligen_091225@sina.cn>
2024-11-08 20:01:06 +0800

    feat: add files

---
+++ 01-helloworld/19-单文件组件/School.vue
@@ -0,0 +1,28 @@
+<template>
+     <!-- 组件的结构 -->
+     <div class="demo">
+        {{name}}
+    </div>
+</template>
+
+<script>
+    // 组件交互相关的代码
+    export default {
+        name: 'School',
+        data() {
+            return {
+                name: "学校"
+            }
+        },
+        methods: {
+            
+        },
+    }
+</script>
+
+<style>
+    /* 组件样式 */
+    .demo {
+        background-color: orange;
+    }
+</style>
